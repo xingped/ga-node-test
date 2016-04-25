9c4e485a6d01311d11b513e40c76aa0635101974
xingped <xingped56@gmail.com>
2016-04-24 19:25:26 -0700

    Added more comments

--- angular/public/js/main.js
+++ angular/public/js/main.js
@@ -1,10 +1,14 @@
+// App and controller are attached to HTML with ng-app and ng-controller attributes
 var myApp = angular.module('myApp', ['ngResource', 'ui.bootstrap']);
 
 myApp.controller('myCtrl', ['$scope', '$resource', '$timeout', function($scope, $resource, $timeout) {
+	// $scope.* variables are accessible in the HTML document this controller is attached to without using the $scope. prefix
 	$scope.page = 'search';
 	$scope.favorites = [];
 	$scope.searchResults = [];
 
+	// $resource objects are an extended object that provides easy Asynchronous calls against APIs
+	// isArray refers to what the returned object from the REST API call is going to be (array or single object)
 	// Resource for accessing our api
 	var Favorites = $resource('/api/favorites/:id', {}, {
 		get: {
@@ -29,12 +33,15 @@ myApp.controller('myCtrl', ['$scope', '$resource', '$timeout', function($scope, 
 		}
 	});
 
+	// Function that is accessible in HTML document
 	$scope.getFavorites = function() {
 		$scope.favorites = [];
-		$scope.favExpand = [];
 		Favorites.get({}, function(data){
 			// Get additional details of each favorited movie and push into favorites list
 			data.forEach(function(movie) {
+				// The first parameter object in a GET/DELETE $resource call is setting url parameters
+				// The second parameter is a success callback function (HTTP Status Code 200)
+				// The third parameter (optional) is an error callback function (Anything status code other than 200)
 				Omdb.get({
 					i: movie.imdbID
 				}, function(data){
@@ -61,6 +68,9 @@ myApp.controller('myCtrl', ['$scope', '$resource', '$timeout', function($scope, 
 	}
 
 	$scope.addFav = function(movie) {
+		// In POST $resource calls, the first parameter is an optional parameter object for setting url parameters
+		// The second parameter is the data passed in a POST request and is displayed here (an object with one parameter called imdbID)
+		// The third and fourth parameters are the success/error callback functions
 		Favorites.post({
 			imdbID: movie.imdbID
 		}, function(data) {
